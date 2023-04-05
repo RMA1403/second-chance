b0658804f0aa5fdbc7ead36475b4bfef1e3d1859
Rava Maulana <ravamaulana14@gmail.com>
2023-04-06 00:52:27 +0700

    initial commit

---
+++ app/profiles/page.tsx
@@ -0,0 +1,3 @@
+export default function ProfilesPage(): JSX.Element {
+  return <h1 className="text-7xl text-green-700">PROFILES</h1>;
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function SignupPage(): JSX.Element {
+  return <h1 className="text-7xl">SIGNUP</h1>;
+}
